30b7f3996b31d801ae6e4e61b65186727bbc533e
Mahesh <mahesh.mathesh@gmail.com>
2025-03-26 16:05:21 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl:
     window.location.hostname === 'localhost'
       ? 'http://localhost:5000/api'
-      : 'https://expense-tracker-api-app.vercel.app/api',
+      : '',
 };
